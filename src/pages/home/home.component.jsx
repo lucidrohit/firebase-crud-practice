6e27a8b09184bdd98faac2ff67a9ecdcb8981219
Rohit Gupta <lucidrohit@gmail.com>
2022-09-04 19:43:30 +0530

    adding loader

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,28 +1,37 @@
 import "./home.styles.scss"
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { getUsers } from "./../../utils/firebase.utils"
+import ClipLoader from "react-spinners/ClipLoader";
 
 function Home() {
+  const divRef = useRef()
   const [users, setUsers] = useState([])
+  const [content, setContent] = useState(null)
+
+
+  const ListUser = (usersList) => <ul style={{ listStyle: "none" }}>
+    {usersList.map(({ name, email, id }) => (
+      <li key={id}>
+        <h1>{name[0].toUpperCase() + name.slice(1)}</h1>
+        <p>{email}</p>
+      </li>
+    ))}
+  </ul>
 
   useEffect(() => {
     const getUsersList = async () => {
       const usersList = await getUsers()
+      setContent(ListUser(usersList))
       setUsers(usersList)
     }
-
     getUsersList()
+
   }, [])
 
+
   return (
-    <div className="home">
-      <ul style={{ listStyle: "none" }}>
-        {users.map(({ name, email, id }) => (
-          <li key={id}>
-            <h1>{name[0].toUpperCase() + name.slice(1)}</h1>
-            <p>{email}</p>
-          </li>))}
-      </ul>
+    <div className="home" ref={divRef}>
+      {content ? content : <ClipLoader size={150} />}
     </div>
   );
 }
