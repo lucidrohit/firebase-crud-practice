71b5655158acd429e9474e2b62ab80296e22a718
Rohit Gupta <lucidrohit@gmail.com>
2022-09-16 14:53:21 +0530

    set home to default(/) route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import './App.scss';
-import { Routes, Route } from "react-router-dom"
-import Home from './pages/home/home.component';
-import RegisterUser from './pages/register/register-user.component';
-import NavBar from './components/navbar/navbar.component';
-import UpdateUser from './pages/update-user/update-user.component';
-import DelUser from './pages/del-user/del-user.component';
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home.component";
+import RegisterUser from "./pages/register/register-user.component";
+import NavBar from "./components/navbar/navbar.component";
+import UpdateUser from "./pages/update-user/update-user.component";
+import DelUser from "./pages/del-user/del-user.component";
 function App() {
-
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<NavBar />}>
-          <Route element={<Home />} path="/home" />
+        <Route path="/" element={<NavBar />}>
+          <Route element={<Home />} path="/" />
           <Route element={<RegisterUser />} path="register-user" />
           <Route element={<UpdateUser />} path="update-user" />
           <Route element={<DelUser />} path="del-user" />
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,37 +1,29 @@
-import { Link } from "react-router-dom"
-import "./navbar.styles.scss"
-import { Outlet } from "react-router-dom"
+import { Link } from "react-router-dom";
+import "./navbar.styles.scss";
+import { Outlet } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <>
-            <div className="navbar">
-                <ul className="navbar__list">
-                    <li className="navbar__list__item">
-                        <Link to="home">
-                            Home
-                        </Link>
-                    </li>
-                    <li className="navbar__list__item">
-                        <Link to="register-user">
-                            Register User
-                        </Link>
-                    </li>
-                    <li className="navbar__list__item">
-                        <Link to="update-user">
-                            Update User
-                        </Link>
-                    </li>
-                    <li className="navbar__list__item">
-                        <Link to="del-user">
-                            Delete User
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-            <Outlet />
-        </>
-    )
-}
+  return (
+    <>
+      <div className="navbar">
+        <ul className="navbar__list">
+          <li className="navbar__list__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navbar__list__item">
+            <Link to="register-user">Register User</Link>
+          </li>
+          <li className="navbar__list__item">
+            <Link to="update-user">Update User</Link>
+          </li>
+          <li className="navbar__list__item">
+            <Link to="del-user">Delete User</Link>
+          </li>
+        </ul>
+      </div>
+      <Outlet />
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
